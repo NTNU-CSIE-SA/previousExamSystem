c1a3f4042b1fcb01741dd161acc16658784ff059
Panda7122 <danal99151@gamil.com>
2025-01-16 17:25:36 +0800

    stole database code from csie camp app

---
+++ src/lib/server/db/control.ts
@@ -0,0 +1,26 @@
+import { db } from "./index";
+
+export async function check_control(email: string) {
+    const control = await db
+        .selectFrom("UserControl")
+        .where(({ or, eb }) => {
+            return or([eb("email", "=", email), eb("email", "=", "fallback")]);
+        })
+        .selectAll()
+        .execute();
+
+    const fallback_control = control.find((c) => c.email === "fallback");
+    const user_control = control.find((c) => c.email === email);
+
+    if (!fallback_control) {
+        throw new Error("No fallback control");
+    }
+
+    if (user_control) {
+        return user_control;
+    } else if (fallback_control) {
+        return fallback_control;
+    } else {
+        throw new Error("No control policy");
+    }
+}
---
+++ src/lib/server/db/index.ts
@@ -0,0 +1,31 @@
+import { env } from "$env/dynamic/private";
+import { sql } from "kysely";
+import { DB } from "sveltekit-db";
+import SQL from "../../../../d1/schema.sql?raw";
+import type { Database } from "./schema";
+
+export const db = DB<Database>();
+
+if (env.DB_COMPONENT === "sqlite") {
+    const tables = await db.introspection.getTables();
+    if (!tables.map((t) => t.name).includes("Application")) {
+        const stmts = SQL.split(";")
+            .map((s) => s.trim())
+            .filter((s) => s.length > 0);
+        for (const stmt of stmts) {
+            console.log(stmt);
+            await sql`${sql.raw(stmt)}`.execute(db);
+        }
+
+        await db
+            .insertInto("UserControl")
+            .values({
+                email: "fallback",
+                can_apply: 1,
+                can_give_up: 1,
+                can_update_additional_info: 1,
+                can_update_profile: 1,
+            })
+            .execute();
+    }
+}
