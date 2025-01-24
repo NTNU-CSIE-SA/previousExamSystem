79047d990879e35026e067eea7c4e4b2ad1a056d
Panda7122 <danal99151@gamil.com>
2025-01-24 21:21:31 +0800

    package prop-types

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,10 +4,10 @@ import React, { useState } from 'react';
 
 //TODO : connect with backend and backend should return a token
 async function loginUser(credentials) {
-    
+
 }
 
-export default function Login(){
+export default function Login() {
 
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
@@ -22,29 +22,29 @@ export default function Login(){
         // that connect with backend and backend should return a token.
     }
 
-    return(
+    return (
         <>
-        <form class="login-box" onSubmit={handleSubmit}>
-            <div class="login-header">
-                <header>Login</header>
-            </div>
-            <div class="input-box">
-                <input type="text" class="input-field" placeholder="StudentID" autocomplete="off" required onChange={e => setUserName(e.target.value)}/>
-            </div>
-            <div class="input-box">
-                <input type="password" class="input-field" placeholder="Password" autocomplete="off" required onChange={e => setPassword(e.target.value)} />
-            </div>
-            <div class="forgot">
-                <section>
-                    <input type="checkbox" id="check" />
-                    <label for="check">Remember me</label>
-                </section>
-            </div>
-            <div class="input-submit">
-                <button type="submit" class="submit-btn" id="submit"></button>
-                <label for="submit">Sign In</label>
-            </div>
-        </form>
+            <form class="login-box" onSubmit={handleSubmit}>
+                <div class="login-header">
+                    <header>Login</header>
+                </div>
+                <div class="input-box">
+                    <input type="text" class="input-field" placeholder="StudentID" autocomplete="off" required onChange={e => setUserName(e.target.value)} />
+                </div>
+                <div class="input-box">
+                    <input type="password" class="input-field" placeholder="Password" autocomplete="off" required onChange={e => setPassword(e.target.value)} />
+                </div>
+                <div class="forgot">
+                    <section>
+                        <input type="checkbox" id="check" />
+                        <label for="check">Remember me</label>
+                    </section>
+                </div>
+                <div class="input-submit">
+                    <button type="submit" class="submit-btn" id="submit"></button>
+                    <label for="submit">Sign In</label>
+                </div>
+            </form>
         </>
     );
 }
