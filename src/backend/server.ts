7b95df426548b4005f838c0bd3cfd02f3798ffb2
Darrin-Lin <darrin.de88@gmail.com>
2025-01-25 00:39:19 +0800

    style: rename BACKEND_PORT.

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -8,7 +8,7 @@ import adminRouter from "./routes/admin";
 import modifyRouter from "./routes/modify_files";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const BACKEND_PORT = process.env.PORT || 5000;
 
 init_server();
 app.use(express.json());
@@ -19,6 +19,6 @@ app.use("/api/view_file", viewFileRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/modify-file", modifyRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(BACKEND_PORT, () => {
+  console.log(`Server is running on port ${BACKEND_PORT}`);
 });
