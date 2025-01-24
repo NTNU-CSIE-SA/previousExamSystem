eff2de8b3feed05678f5dec3e55ae247a44eea92
Darrin-Lin <darrin.de88@gmail.com>
2025-01-24 23:39:57 +0800

    feat: add modify file.

---
+++ src/backend/routes/modify_files.ts
@@ -0,0 +1,235 @@
+import express, { Request, Response } from 'express';
+import fs from 'fs';
+import { school_id_from_token } from './auth';
+import { check_admin_level } from './admin';
+import { db } from '../db';
+
+let VIEW_UNVERIFIED_LEVEL = 2;
+if (process.env.VERIFY_LEVEL !== undefined) {
+    VIEW_UNVERIFIED_LEVEL = parseInt(process.env.VERIFY_LEVEL);
+    if (isNaN(VIEW_UNVERIFIED_LEVEL)) {
+        VIEW_UNVERIFIED_LEVEL = 2;
+    }
+}
+
+let MODIFY_FILES_LEVEL = 4;
+if (process.env.MODIFY_FILES_LEVEL !== undefined) {
+    MODIFY_FILES_LEVEL = parseInt(process.env.MODIFY_FILES_LEVEL);
+    if (isNaN(MODIFY_FILES_LEVEL)) {
+        MODIFY_FILES_LEVEL = 4;
+    }
+}
+
+let VERIFIED_DIR = '../../verfied';
+if (process.env.VERIFIED_DIR !== undefined) {
+    VERIFIED_DIR = process.env.VERIFIED_DIR;
+}
+
+let UPLOADS_DIR = './uploads';
+if (process.env.UPLOADS_DIR !== undefined) {
+    UPLOADS_DIR = process.env.UPLOADS_DIR;
+}
+
+const router = express.Router();
+
+router.post('/verify', async (req: Request, res: Response) => {
+    try{
+        const school_id = await school_id_from_token(req, res);
+        if (!school_id) {
+            res.status(401).json({ message: 'Invalid token' });
+            return;
+        }
+        const admin_level = await check_admin_level(school_id);
+        if (!admin_level) {
+            res.status(401).json({ message: 'Invalid school ID' });
+            return;
+        }
+        if (admin_level < VIEW_UNVERIFIED_LEVEL) {
+            res.status(403).json({ message: 'Permission denied' });
+            return;
+        }
+        const file_id = req.body.file_id;
+        if (!file_id) {
+            res.status(400).json({ message: 'File ID is required' });
+            return;
+        }
+        const file = await db
+            .selectFrom('Document')
+            .selectAll()
+            .where('id', '=', file_id)
+            .executeTakeFirst();
+        if (!file) {
+            res.status(404).json({ message: 'File not found' });
+            return;
+        }
+        if (file.verified) {
+            res.status(400).json({ message: 'File is already verified' });
+            return;
+        }
+        if (!fs.existsSync(`${UPLOADS_DIR}/${file.pdf_locate}`)) {
+            res.status(404).json({ message: 'File not found' });
+            return;
+        }
+        if (!fs.existsSync(`${VERIFIED_DIR}`)) {
+            fs.mkdirSync(`${VERIFIED_DIR}`, { recursive: true });
+        }
+        fs.rename(`${UPLOADS_DIR}/${file.pdf_locate}`, `${VERIFIED_DIR}/${file.id}`, (err) => { 
+            if (err) {
+                console.error(err);
+                res.status(500).json({ message: 'Internal server error' });
+                return;
+            }
+        });
+        await db
+            .updateTable('Document')
+            .set({ verified: 1 , pdf_locate: `${file.id}`})
+            .where('id', '=', file_id)
+            .execute();
+        res.json({ message: 'File verified' });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+router.post('/delete', async (req: Request, res: Response) => {
+    try{
+        const school_id = await school_id_from_token(req, res);
+        if (!school_id) {
+            res.status(401).json({ message: 'Invalid token' });
+            return;
+        }
+        const admin_level = await check_admin_level(school_id);
+        if (!admin_level) {
+            res.status(401).json({ message: 'Invalid school ID' });
+            return;
+        }
+        if (admin_level < MODIFY_FILES_LEVEL) {
+            res.status(403).json({ message: 'Permission denied' });
+            return;
+        }
+        const file_id = req.body.file_id;
+        if (!file_id) {
+            res.status(400).json({ message: 'File ID is required' });
+            return;
+        }
+        const file = await db
+            .selectFrom('Document')
+            .selectAll()
+            .where('id', '=', file_id)
+            .executeTakeFirst();
+        if (!file) {
+            res.status(404).json({ message: 'File not found' });
+            return;
+        }
+        if (file.verified) {
+            res.status(400).json({ message: 'Cannot delete verified file' });
+            return;
+        }
+        if (!fs.existsSync(`${UPLOADS_DIR}/${file.pdf_locate}`)) {
+            res.status(404).json({ message: 'File not found' });
+            return;
+        }
+        fs.unlink(`${UPLOADS_DIR}/${file.pdf_locate}`, (err) => { 
+            if (err) {
+                console.error(err);
+                res.status(500).json({ message: 'Internal server error' });
+                return;
+            }
+        });
+        await db
+            .deleteFrom('Document')
+            .where('id', '=', file_id)
+            .execute();
+        res.json({ message: 'File deleted' });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+router.post('/modify-file-info', async (req: Request, res: Response) => {
+    try{
+        const school_id = await school_id_from_token(req, res);
+        if (!school_id) {
+            res.status(401).json({ message: 'Invalid token' });
+            return;
+        }
+        const admin_level = await check_admin_level(school_id);
+        if (!admin_level) {
+            res.status(401).json({ message: 'Invalid school ID' });
+            return;
+        }
+        if (admin_level < MODIFY_FILES_LEVEL) {
+            res.status(403).json({ message: 'Permission denied' });
+            return;
+        }
+        const file_id = req.body.file_id;
+        if (!file_id) {
+            res.status(400).json({ message: 'File ID is required' });
+            return;
+        }
+        const file = await db
+            .selectFrom('Document')
+            .selectAll()
+            .where('id', '=', file_id)
+            .executeTakeFirst();
+        if (!file) {
+            res.status(404).json({ message: 'File not found' });
+            return;
+        }
+        const subject = req.body.subject;
+        const semester = req.body.semester;
+        const exam_type = req.body.exam_type;
+        const verfied = req.body.verified;
+        if (file.verified === 1 && verfied === 0) {
+            if (!fs.existsSync(`${VERIFIED_DIR}/${file.id}`)) {
+                res.status(404).json({ message: 'File not found' });
+                return;
+            }
+            if (!fs.existsSync(`${UPLOADS_DIR}`)) {
+                fs.mkdirSync(`${UPLOADS_DIR}`, { recursive: true });
+            }
+            fs.rename(`${VERIFIED_DIR}/${file.id}`, `${UPLOADS_DIR}/${file.pdf_locate}`, (err) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).json({ message: 'Internal server error' });
+                    return;
+                }
+            });
+        }
+        else if (file.verified === 0 && verfied === 1) {
+            if (!fs.existsSync(`${UPLOADS_DIR}/${file.pdf_locate}`)) {
+                res.status(404).json({ message: 'File not found' });
+                return;
+            }
+            if (!fs.existsSync(`${VERIFIED_DIR}`)) {
+                fs.mkdirSync(`${VERIFIED_DIR}`, { recursive: true });
+            }
+            fs.rename(`${UPLOADS_DIR}/${file.pdf_locate}`, `${VERIFIED_DIR}/${file.id}`, (err) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).json({ message: 'Internal server error' });
+                    return;
+                }
+            });
+        }
+        await db
+            .updateTable('Document')
+            .$if(subject !== undefined, (qb) => qb.set({ subject: subject }))
+            .$if(semester !== undefined, (qb) => qb.set({ semester: semester }))
+            .$if(exam_type !== undefined, (qb) => qb.set({ exam_type: exam_type }))
+            .$if(verfied !== undefined, (qb) => qb.set({ verified: verfied }))
+            .where('id', '=', file_id)
+            .execute();
+        res.json({ message: 'File info modified' });
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+export default router;
--- src/backend/server.ts
+++ src/backend/server.ts
@@ -5,6 +5,7 @@ import uploadRouter from "./routes/upload_file";
 import viewFileRouter from "./routes/view_file";
 import filterRouter from "./routes/filter";
 import adminRouter from "./routes/admin";
+import modifyRouter from "./routes/modify_files";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,6 +17,7 @@ app.use("/api/filter", filterRouter);
 app.use("/api/upload_file", uploadRouter);
 app.use("/api/view_file", viewFileRouter);
 app.use("/api/admin", adminRouter);
+app.use("/modify-file", modifyRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
