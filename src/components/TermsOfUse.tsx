5f0158535527bc37e7bc0255f85240d1eb3876cb
Darrin-Lin <darrin.de88@gmail.com>
2025-02-22 19:55:24 +0800

    refactor: TermsOfUse.tsx.

--- src/components/TermsOfUse.tsx
+++ src/components/TermsOfUse.tsx
@@ -1,6 +1,7 @@
+import React from 'react'
 import '../style/docs.css'
-export default function TermsOfUse(){
-    return(
+export default function TermsOfUse() {
+    return (
         <div className='TermsOfUse-Box'>
             <h1>服務條款</h1>
         </div>
