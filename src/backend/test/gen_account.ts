02d7f6995113825d3824e46d6e2f1b6ecb273196
Darrin-Lin <darrin.de88@gmail.com>
2025-02-23 16:45:11 +0800

    feat: don't add exsist school_id.

--- src/backend/test/gen_account.ts
+++ src/backend/test/gen_account.ts
@@ -3,7 +3,6 @@ import fs from 'fs';
 import readline from 'readline';
 import sqlite3 from 'better-sqlite3';
 
-// 連接 SQLite
 const db = new sqlite3('db.sqlite');
 
 const csvFilePath = './src/backend/test/account.csv';
@@ -17,13 +16,13 @@ async function importCSVToSQLite() {
 
     let headers: string[] = [];
     const insertSQL = db.prepare(
-        'INSERT INTO Profile (school_id, name, password, ban_until, admin_level) VALUES (?, ?, ?, ?, ?)' 
+        'INSERT INTO Profile (school_id, name, password, ban_until, admin_level) VALUES (?, ?, ?, ?, ?)'
     );
-    
+
     db.transaction(() => {
         rl.on('line', (line) => {
             const values = line.split(',');
-            
+
             if (headers.length === 0) {
                 headers = values.map(header => header.trim());
             } else {
@@ -32,10 +31,12 @@ async function importCSVToSQLite() {
                     row[header] = values[index]?.trim();
                 });
 
-                // 插入 SQLite
-                insertSQL.run(row.school_id, row.name === '' ? null : row.name
-                    , bcrypt.hashSync(row.password, 10), row.ban_until === '' ? null : row.ban_until
-                    , parseInt(row.admin_level, 10));
+                // don't insert school_id's already in SQL
+                if (db.prepare('SELECT * FROM Profile WHERE school_id = ?').get(row.school_id) === undefined) {
+                    insertSQL.run(row.school_id, row.name === '' ? null : row.name
+                        , bcrypt.hashSync(row.password, 10), row.ban_until === '' ? null : row.ban_until
+                        , parseInt(row.admin_level, 10));
+                }
             }
         });
     })();
